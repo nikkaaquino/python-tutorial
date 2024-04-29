1cfa1a0cd6047cd5a93627cb131475ee52a79610
Nikka Aquino <nikkaaquino31@gmail.com>
2024-04-29 21:31:53 +0800

    List

--- app.py
+++ app.py
@@ -323,6 +323,7 @@ for x in range(4):
 '''
 
 #Challenge
+'''
 numbers = [5, 2, 5, 2, 2]
 
 for x_count in numbers:
@@ -332,7 +333,122 @@ for x_count in numbers:
     print(output)
     #print('x' * x)
    
+'''
 
+#List
+'''
+names = ['John', 'Bob', 'Mosh', 'Sarah', 'Mary']
+print(names[2:4])
+'''
 
+#Find the largest number
+'''
+numbers = [3, 6, 2, 8, 4, 10]
+max = numbers[0]
 
+for number in numbers:
+    if number > max:
+        max = number
+print(max)
+'''
 
+#2D Lists
+'''
+[
+    1 2 3
+    4 5 6
+    7 8 9
+]
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+#print(matrix[0][1])
+#matrix[0][1] = 20
+#print(matrix[0][1])
+
+for row in matrix:
+    for item in row:
+        print(item)
+'''
+
+#List Methods
+'''
+numbers = [5, 2, 1, 7, 4]
+#numbers.insert(0, 10)
+#numbers.remove(5)
+#numbers.clear()
+#numbers.pop() //remove the last value from the list
+#print(numbers)
+#print(numbers.index(5))
+#print(50 in numbers) #false - occurunces in list
+#print(numbers.count(5))
+#numbers.sort()
+#numbers.reverse()
+
+numbers2 = numbers.copy()
+numbers.append(10)
+print(numbers)
+print(numbers2)
+'''
+
+#Exercise
+'''
+numbers = [1,2,1,2,3,4,5]
+uniques = []
+
+for number in numbers:
+    if number not in uniques:
+        uniques.append(number)
+print(uniques)
+'''
+
+#Tupples
+'''
+numbers = (1,2,3)
+print(numbers[0])
+'''
+
+#Unpacking
+'''
+coordinates = (1, 2, 3)
+#x = coordinates[1]
+#y = coordinates[1]
+#z = coordinates[2]
+
+x, y, z = coordinates
+print(x)
+'''
+
+#Dictionaries
+'''
+customer = {
+    "name": "John Smith",
+    "age" : 30,
+    "is_verified": True
+}
+
+#print(customer["name"])
+customer["name"] = "Jack Smith"
+print(customer.get("birthdate", "Jan 1 1980"))
+print(customer["name"])
+
+'''
+
+#Exercise
+phone = input("Phone: ")
+digits_mapping = {
+    "1" : "One",
+    "2" : "Two",
+    "3" : "Three",
+    "4" : "Four"
+}
+
+output = ""
+
+for ch in phone:
+    output+= digits_mapping.get(ch, "!") + " "
+print(output)
