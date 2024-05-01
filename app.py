c8401ba3d1c5804472323aca22432e7d6e0e559b
Nikka Aquino <nikkaaquino31@gmail.com>
2024-05-01 16:10:47 +0800

    Working with Directories, Pypi and Pip, Automation with Python

--- app.py
+++ app.py
@@ -681,3 +681,59 @@ print(dice.roll())
 '''
 
 #Working with Directories
+'''
+from pathlib import Path
+
+#Absolute Path
+# c:\Program Files\Microsoft
+#/usr/local/bin - linux
+#Relative Path
+
+path = Path()
+
+for file in path.glob('*.py'):
+    print(file)
+
+#print(path.rmdir()) #remove directory
+#print(path.mkdir())
+#print(path.exists())
+'''
+
+#Pypi and Pip
+#pip install openpyxl
+
+# Automation with Python
+
+import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
+
+def process_workbook(filename):
+    wb = xl.load_workbook(filename)
+    sheet = wb['Sheet1']
+    #cell = sheet['a1']
+    #sheet.cell(1, 1)
+
+    #print(cell.value)
+    #print(sheet.max_row)
+
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row, 3)
+        corrected_price = cell.value * 0.9
+
+        corrected_price_cell = sheet.cell(row, 4)
+        corrected_price_cell.value =corrected_price
+
+    values = Reference(sheet, 
+            min_row = 2, 
+            max_row = sheet.max_row,
+            min_col = 4,
+            max_col = 4)
+
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, 'e2')
+
+    wb.save(filename)
+        #print(cell.value)
+        #print(row)
+
